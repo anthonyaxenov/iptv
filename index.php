16e92f8c087c890ca7b8285c9efef4b0e6c1bc1e
AnthonyAxenov (VPS sites) <anthonyaxenov@gmail.com>
2020-04-20 12:02:34 +0800

    Куча мелких правок
    - Файл channels.ini переименован в playlists.ini, добавлена текстовка
    - Добавлена ссылка на гитхаб
    - Добавлена дата посл. изменения файла playlists.ini
    - Не выводятся те плейлисты из файла, у коих не указан pls
    - Убран закомментированный код

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if ($_GET['dbg']) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
+/**
+ * Возвращает количество каналов в плейлисте
+ * 
+ * @param string $pls_url URL плейлиста
+ * @return int
+ */
 function getChannelCount($pls_url) {
     $content = file_get_contents($pls_url);
     $matches = [];
@@ -11,17 +19,12 @@ function getChannelCount($pls_url) {
     return count($matches[0]);
 }
 
+// Шаблон короткой ссылки на плейлист
 // $my_url = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'?s=';
 $my_url = $_SERVER['SERVER_NAME'].'/'.basename(__DIR__).'?s=';
 
-$data = parse_ini_file('channels.ini', true);
-
-if (!empty($_GET['dbg'])) {
-    var_dump(__DIR__);
-    var_dump(basename(__DIR__));
-    var_dump($_SERVER);
-    die;
-}
+// Чтение списка плейлистов из ini-файла
+$data = parse_ini_file('playlists.ini', true);
 
 if (empty($_GET['s'])) { ?>
     <!DOCTYPE html>
@@ -34,15 +37,18 @@ if (empty($_GET['s'])) { ?>
     <body>
         <h1>Список самообновляемых плейлистов для IPTV</h1>
         <p>
-            Эти плейлисты бесплатны и подобраны вручную из открытых источников.<br>
-            Поддержкой самих плейлистов занимаются сервисы и ресурсы, указанные как источник (если таковые имеются).
+            Дата обновления списка: <strong>
+                <?=date('d-m-Y h:i:s', filemtime('playlists.ini'))?>
+                <br>
+                <a href="https://github.com/anthonyaxenov/iptv">github.com/anthonyaxenov/iptv</a>
+            </strong>
+        <p>
+            Поддержкой этих плейлистов занимаются сервисы и ресурсы, указанные как источник (если таковые имеются).<br>
+            Эти плейлисты собраны здесь вручную и бесплатны.
         </p>
         <p>Чтобы подключить плейлист, нужно в настройках IPTV-плеера добавить ссылку в следующем формате:</p>
         <pre><?=$my_url?><strong>ID</strong></pre>
-        <p>
-            где <strong>ID</strong> - один из указанных ниже идентификаторов.<br>
-            Либо указывать ссылку на оригинальный плейлист в последней колонке.
-        </p>
+        <p>где <strong>ID</strong> - один из указанных ниже идентификаторов.</p>
         <table width="100%" border="1" cellpadding="1">
             <thead>
                 <tr>
@@ -50,42 +56,35 @@ if (empty($_GET['s'])) { ?>
                     <td>Название, источник</td>
                     <td style="text-align: center">Каналов</td>
                     <td>Плейлист</td>
-                    <!-- <td>Оригинал</td> -->
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($data as $id => $element) { ?>
-                <tr>
-                    <td style="text-align: center">
-                        <strong><?=$id?></strong>
-                    </td>
-                    <td>
-                        <strong>
-                            <?php if (empty($element['src'])) { ?>
-                                <?=$element['name']?>
-                            <?php } else { ?>
-                                <a href="<?=$element['src']?>" target="_blank" rel="noopener nofollow"><?=$element['name']?></a>
-                            <?php } ?>
-                        </strong>
-                    </td>
-                    <td style="text-align: center"><?=getChannelCount($element['pls'])?></td>
-                    <td onclick="prompt('Скопируйте адрес плейлиста', '<?=$my_url?><?=$id?>')"
-                        title="Нажмите, чтобы скопировать адрес"
-                        style="cursor:pointer">
-                        <strong>
-                            <pre><?=$my_url?><?=$id?></pre>
-                        </strong>
-                    </td>
-                    <?php
-                    /*
-                    <td onclick="prompt('Скопируйте адрес плейлиста', '<?=$element['pls']?>')"
-                        title="Нажмите, чтобы скопировать адрес"
-                        style="cursor:pointer">
-                        <pre><?=$element['pls']?></pre>
-                    </td>
-                    */
-                    ?>
-                </tr>
+                <?php foreach ($data as $id => $element) { 
+                    if (empty($element['pls'])) {
+                        continue;
+                    } ?>
+                    <tr>
+                        <td style="text-align: center">
+                            <strong><?=$id?></strong>
+                        </td>
+                        <td>
+                            <strong>
+                                <?php if (empty($element['src'])) { ?>
+                                    <?=$element['name']?>
+                                <?php } else { ?>
+                                    <a href="<?=$element['src']?>" target="_blank" rel="noopener nofollow"><?=$element['name']?></a>
+                                <?php } ?>
+                            </strong>
+                        </td>
+                        <td style="text-align: center"><?=getChannelCount($element['pls'])?></td>
+                        <td onclick="prompt('Скопируйте адрес плейлиста', '<?=$my_url?><?=$id?>')"
+                            title="Нажмите, чтобы скопировать адрес"
+                            style="cursor:pointer">
+                            <strong>
+                                <pre><?=$my_url?><?=$id?></pre>
+                            </strong>
+                        </td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
