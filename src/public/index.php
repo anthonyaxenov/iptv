11b45444c735cefee4d2e6c3c31b2ffd5194fe64
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-09-01 23:57:29 +0800

    Перевёрстан faq, удалены лишние файлы, composer-скрипнт для очистки кеша twig

--- src/public/index.php
+++ src/public/index.php
@@ -2,12 +2,28 @@
 
 declare(strict_types = 1);
 
+use App\Core\Bootstrapper;
+
 /*
 |--------------------------------------------------------------------------
 | Bootstrap all classes, settings, etc.
 |--------------------------------------------------------------------------
 */
-require '../bootstrap.php';
+
+// autoload composer packages
+require '../vendor/autoload.php';
+
+// load .env parameters
+Bootstrapper::bootEnv();
+
+// set up framework according to its config
+Bootstrapper::bootSettings();
+
+// set up Twig template engine
+Bootstrapper::bootTwig();
+
+// set up routes defined in config file
+Bootstrapper::bootRoutes();
 
 /*
 |--------------------------------------------------------------------------
