944fbdcfceca4803189a49c39cb328f9b6dfaf11
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-10-22 16:45:58 +0800

    Fixed download-all.sh

--- tools/download-all.sh
+++ tools/download-all.sh
@@ -16,7 +16,17 @@
 #
 #################################################
 
-rm -rf ./downloaded
-mkdir -p ./downloaded && \
-    cd ./downloaded && \
-    grep -P "pls='(.*)'" ../playlists.ini | sed "s/^pls=//g" | sed "s/'//g" | tr -d '\r' | xargs wget
+TOOLS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+DL_DIR="$TOOLS_DIR/downloaded"
+ROOT_DIR="`dirname "$TOOLS_DIR"`"
+INI_FILE="$ROOT_DIR/playlists.ini"
+
+rm -rf "$DL_DIR" && \
+    mkdir -p "$DL_DIR" && \
+    cd "$DL_DIR" && \
+        cat "`dirname "$TOOLS_DIR"`/playlists.ini" \
+        | grep -P "pls\s*=\s*'(.*)'" \
+        | sed "s#^pls\s*=\s*##g" \
+        | sed "s#'##g" \
+        | tr -d '\r' \
+        | xargs wget
